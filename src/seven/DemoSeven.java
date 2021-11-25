e9b50e1840d7cddb36c1f054de01c862a33303f2
gmena730 <65830632+gmena730@users.noreply.github.com>
2021-11-24 21:09:50 -0400

    Commit Demo Seven

---
+++ src/seven/DemoSeven.java
@@ -0,0 +1,13 @@
+package seven;
+
+public class DemoSeven {
+	
+	public static void main(String[] args) {
+		System.out.println("Demo Six");
+	}
+	
+	public static void addNum(int a, int b) {
+		System.out.println(a + b);
+	}
+
+}
